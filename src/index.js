9986c184c319692f204ac3feac2e8d246f157494
Prabhat Patel <prabhatpatel508@outlook.com>
2023-03-18 10:48:46 +0530

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+if (typeof window !== 'undefined') {
+    console.log('we are running on the client')
+} else {
+    console.log('we are running on the server');
+}
 root.render(
   <App/>
 );
